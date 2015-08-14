4d0476c58408936389fa50ecb5f082fa9d1cfd75
pu1s <pu1s@outlook.com>
2015-08-14 21:32:45 +0300

    Помощь рабочая неделя

--- WorkCalendarCoreLibrary/WorkCalendar/Core/CalendarWorkWeek.cs
+++ WorkCalendarCoreLibrary/WorkCalendar/Core/CalendarWorkWeek.cs
@@ -1,13 +1,22 @@
 namespace AGSoft.WorkCalendar.Core
 {
     /// <summary>
-    /// 
+    /// Рабочая неделя
     /// </summary>
  
     public enum CalendarWorkWeek
     {
+        /// <summary>
+        /// Пятидневная
+        /// </summary>
         FiveDaysWorkWeek = 0,
+        /// <summary>
+        /// Шестидневная
+        /// </summary>
         SixDaysWorkWeek,
+        /// <summary>
+        /// Неопределенная
+        /// </summary>
         UndefinedWorkWeek
     };
 }
