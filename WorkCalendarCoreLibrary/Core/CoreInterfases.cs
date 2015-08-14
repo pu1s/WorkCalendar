3cff902d887026c0d55a9cf308d60a35196f2c92
pu1s <pu1s@outlook.com>
2015-08-14 17:07:45 +0300

    число, уникальный индентификатор календарного дня

--- WorkCalendarCoreLibrary/Core/CoreInterfases.cs
+++ WorkCalendarCoreLibrary/Core/CoreInterfases.cs
@@ -200,11 +200,19 @@ namespace WorkCalendarCoreLibrary.Core
         #endregion
 
     }
-
+    /// <summary>
+    /// Определяет уникальность календарного дня
+    /// </summary>
     public static class CalendarDayHandle
     {
+        /// <summary>
+        /// Опеределяет уникальный индентификатор календарного дня
+        /// </summary>
+        /// <param name="date">День</param>
+        /// <returns>число, уникальный индентификатор календарного дня</returns>
         public static int SetCalendarDayHandle(DateTime date)
         {
+            // Вычисляем целое число, уникальный индентификатор календарного дня
             return ((date.Year*1000) + date.DayOfYear);
         }
     }
