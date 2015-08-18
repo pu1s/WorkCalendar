3ae5001684889e931bbed36916ad518b788b4f35
pu1s <pu1s@outlook.com>
2015-08-18 09:02:59 +0300

    словарь

---
+++ WorkCalendarCoreLibrary/OrdinaryCalendar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AGSoft
+{
+    public static class OrdinaryCalendar
+    {
+        public static void GetCalendar()
+        {
+            Dictionary<string, string> yearDictionary;
+            yearDictionary = new Dictionary<string, string>();
+            yearDictionary.Add("01.01", "Новый год!");
+            yearDictionary.Add("23.02", "День защитника отечества");
+            yearDictionary.Add("08.03", "Международный женский день");
+        }
+        
+        
+    }
+}
