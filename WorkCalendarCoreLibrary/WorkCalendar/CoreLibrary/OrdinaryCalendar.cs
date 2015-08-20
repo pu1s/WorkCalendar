403cc200510103953d6db0fc9be454590f9323a5
Горин Александр <pu1s@outlook.com>
2015-08-21 00:47:11 +0300

    //********************************************************** // Базовые классы, перечисления и интерфейсы для работы // с производственным календарем // MS Framework .Net 4.5 // С# version 6.0 // Горин Александр 19.08.2015 // Alex Gorin Software 2015 //*********************************************************

--- WorkCalendarCoreLibrary/WorkCalendar/CoreLibrary/OrdinaryCalendar.cs
+++ WorkCalendarCoreLibrary/WorkCalendar/CoreLibrary/OrdinaryCalendar.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections.Generic;
+﻿//**********************************************************
+// Базовые классы, перечисления и интерфейсы для работы 
+// с производственным календарем 
+// MS Framework .Net 4.5
+// С# version 6.0
+// Горин Александр 19.08.2015
+// Alex Gorin Software 2015
+//*********************************************************
+using System.Collections.Generic;
 
 namespace AGSoft.WorkCalendar.CoreLibrary
 {
