f0c66106c69fc7937e7dbf74e01b18e76263c5ad
pu1s <pu1s@outlook.com>
2015-08-22 19:19:34 +0300

    Начало контрола

--- WorkCalendar/FormMain.cs
+++ WorkCalendar/FormMain.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 
 namespace WorkCalendar
 {
     public partial class FormMain : Form
     {
-        [DllImport("AuthLib.dll", EntryPoint = "GetVersionInfo", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
-        public static extern void GetVersionInfo(out int a);
-
-        private int _a = 0;
+        
         public FormMain()
         {
             InitializeComponent();
@@ -26,8 +14,7 @@ namespace WorkCalendar
         
         private void button1_Click(object sender, EventArgs e)
         {
-            GetVersionInfo(out _a);
-            button1.Text = _a.ToString();
+            
         }
     }
 }
