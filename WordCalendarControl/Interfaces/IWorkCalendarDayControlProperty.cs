7b71038b320e2aa4ccafb8f63b33035b2be124b6
pu1s <pu1s@outlook.com>
2015-08-22 19:44:15 +0300

    Добавлены свойства календарного дня

---
+++ WordCalendarControl/Interfaces/IWorkCalendarDayControlProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AGSoft.WorkCalendarControl.Interfaces
+{
+    public interface IWorkCalendarDayControlProperty
+    {
+        bool IsDescripted { get; set; }
+        bool IsCommented { get; set; }
+        bool IsAttributed { get; set; }
+
+        event EventHandler ChangeWorkCalendarDayProperty;
+    }
+}
+
---
+++ WordCalendarControl/WorkCalendarDayControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AGSoft.WorkCalendar.CoreLibrary;
+using AGSoft.WorkCalendarControl.Interfaces;
+
+namespace AGSoft.WorkCalendarControl
+{
+    public partial class WorkCalendarDayControl : UserControl
+    {
+        public WorkCalendarDayControl()
+        {
+            InitializeComponent();
+        }
+
+        public CalendarDay CalendarDay { get; }
+        public override Color BackColor { get; set; }
+        public override Font Font { get; set; }
+    }
+}
