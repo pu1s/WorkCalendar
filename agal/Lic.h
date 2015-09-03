a50845836f3d45d2c49ed593dca65d93462a5f71
Горин Александр <pu1s@outlook.com>
2015-09-03 11:27:38 +0300

    New fix

--- agal/Lic.h
+++ agal/Lic.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <string>
 using namespace std;
-//#define LIC_H
-
+#define _AGS_FF *u32string
+#define _UName &uname
+#define _UEmail &uemail
+#define _ASTRING string
+#define pixel (int*)
+#define _ww_args unsigned int
+typedef int* _WW_AST;
+typedef struct {
+	unsigned x;
+	unsigned y;
+} POINT;
 
 namespace AGSoft
 {
@@ -15,6 +24,21 @@ namespace AGSoft
 		void Encode(string &word);
 		void Decode(string &word);
 		void GenerateLic(string *uname, string *email);
+	private:
+		char *_name;
+		const string _WIN_A = "TestStr";
+	
+	public:
+		int *protected_FF;
+		string *str;
+		
+
+		
+		_ASTRING GetA(_ASTRING & str);
+		_WW_AST ImplFunc(_ASTRING * _UEmale, _ASTRING * _UName);
+		_WW_AST Set_WW_AST(int* i);
+		int URS_intPtr(int** args);
+		POINT GetPoint(_ww_args x, _ww_args y);
 	};
 }
 
--- agal/Source.c
+++
@@ -1,7 +0,0 @@
-//#include <cstdio>
-//#include <vector>
-#include "Header.h"
-#define _WIN_32 
-#ifdef _WIN_32
-#include <Windows.h>
-#endif
