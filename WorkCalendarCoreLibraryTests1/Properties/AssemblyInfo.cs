d1b4b81c0b19c530e5e4fe4245b1c2b2b7d47fbc
Горин Александр <pu1s@outlook.com>
2015-09-07 10:31:53 +0300

    Добавлено тестирование

---
+++ WorkCalendarCoreLibraryTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WorkCalendarCoreLibraryTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkCalendarCoreLibraryTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("1da82c5b-1cfe-4b48-ad21-d6871adeccb0")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WorkCalendarCoreLibraryTests1/WorkCalendar/CoreLibrary/HollydaysInfoTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AGSoft.WorkCalendar.CoreLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkCalendarCoreLibraryTests1.WorkCalendar.CoreLibrary
+{
+    [TestClass()]
+    public class HollydaysInfoTests
+    {
+        [TestMethod()]
+        public void GetCalendarDayDescriptionAndAttributeTest()
+        {
+           var a1 = new CalendarDay(DateTime.Now);
+            HollydaysInfo.GetCalendarDayDescriptionAndAttribute(new DateTime(DateTime.Now.Year, 01, 01), ref a1);
+            Assert.AreEqual<CalendarDay>(a1, new CalendarDay(new DateTime(DateTime.Now.Year, 01, 01)));
+        }
+    }
+}
